42c021e9051b8735a2162e1342e1d69cf9676797
Justin Thai <justinthai.x@gmail.com>
2025-10-16 23:11:39 +0000

    title & canvas implementation (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+// title
+const headingText = "Sticker Sketchpad";
+const heading = document.createElement("h1");
+heading.textContent = headingText;
+document.body.appendChild(heading);
+
+// init canvas
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+if (!ctx) { //ctx error check
+  console.error("Failed to get 2D rendering context");
+  throw new Error("Canvas 2D context not supported");
+}
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
